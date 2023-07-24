a44918675c572f46f607d11a9b2d50dade9b52dd
NotLester <lester.lewisudupi@gmail.com>
2023-07-24 09:42:39 +0530

    added rate limiter and proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,8 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT,
-    SALT_ROUNDS: process.env.SALT_ROUNDS,
-    JWT_EXPIRY: process.env.JWT_EXPIRY,
-    JWT_SECRET: process.env.JWT_SECRET,
-}
+	PORT: process.env.PORT,
+	SALT_ROUNDS: process.env.SALT_ROUNDS,
+	JWT_EXPIRY: process.env.JWT_EXPIRY,
+	JWT_SECRET: process.env.JWT_SECRET,
+	FLIGHT_SERVCE: process.env.FLIGHT_SERVICE,
+	BOOKING_SERVICE: process.env.BOOKING_SERVICE,
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	max: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes)
+});
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
+app.use(
+	'/flightsService',
+	createProxyMiddleware({
+		target: ServerConfig.FLIGHT_SERVCE,
+		changeOrigin: true,
+	})
+);
+app.use(
+	'/bookingService',
+	createProxyMiddleware({
+		target: ServerConfig.BOOKING_SERVICE,
+		changeOrigin: true,
+	})
+);
 
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+	console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
